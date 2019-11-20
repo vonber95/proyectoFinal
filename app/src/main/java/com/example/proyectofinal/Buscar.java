4d2535d63e09b3f78dd2984ef2d47541e4f174cc
Marcelo von Bertrab Bortoni <marcelovonbertrabbortoni@Marcelos-MacBook-Pro-2.local>
2019-11-19 18:45:35 -0600

    video de youtube

--- app/src/main/java/com/example/proyectofinal/Buscar.java
+++ app/src/main/java/com/example/proyectofinal/Buscar.java
@@ -3,12 +3,58 @@ package com.example.proyectofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+import android.content.Intent;
 
-public class Buscar extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class Buscar extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_REQUEST = 1;
+    private YouTubePlayerView youTubeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored){
+        if(!wasRestored){
+            //Cambiar esto para el video que se necesite
+            player.cueVideo("fhWaJi1Hsfo");  // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason){
+        if (errorReason.isUserRecoverableError()){
+            errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
+        } else {
+            String error = String.format(getString(R.string.player_error), errorReason.toString());
+            Toast.makeText(this,error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == RECOVERY_REQUEST){
+            getYouTubePlayerProvider().initialize(Config.YOUTUBE_API_KEY, this);
+        }
+    }
+
+    protected Provider getYouTubePlayerProvider() {
+        return youTubeView;
     }
 }
+
+//URL de tutorial Youtube
+// https://www.sitepoint.com/using-the-youtube-api-to-embed-video-in-an-android-app/
---
+++ app/src/main/java/com/example/proyectofinal/Config.java
@@ -0,0 +1,11 @@
+package com.example.proyectofinal;
+
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyD9cTgECiAfBYnEZfxTYHqSSLhbWLkeSpI";
+
+}
