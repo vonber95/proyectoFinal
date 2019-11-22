25a6bd975289552f789be7a7365f0d7478eaf29f
Marcelo von Bertrab Bortoni <marcelovonbertrabbortoni@Marcelos-MacBook-Pro-2.local>
2019-11-22 09:58:49 -0600

    Favorito

--- app/src/main/java/com/example/proyectofinal/Artista.java
+++ app/src/main/java/com/example/proyectofinal/Artista.java
@@ -3,12 +3,26 @@ package com.example.proyectofinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Artista extends AppCompatActivity {
+
+    ImageButton imgButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artista);
+
+        imgButton = (ImageButton) findViewById(R.id.imageB);
+
+        imgButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                imgButton.setImageResource(R.drawable.ic_heart_full);
+            }
+        });
     }
 }
